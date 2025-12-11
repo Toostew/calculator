d33aa1eb2eb55b3f2b73d9c3644e37deaaa40ae6
Toostew <tookaystewie@gmail.com>
2025-12-11 15:22:32 +0800

    basic functionality achieved

--- src/main/java/org/tookaystewie/controllers/calculatorViewController.java
+++ src/main/java/org/tookaystewie/controllers/calculatorViewController.java
@@ -22,7 +22,6 @@ public class calculatorViewController {
     private int multiplyer = 10;
     private int currentNumber = 0;
     private LinkedList<String> firstInput = new  LinkedList<>();
-    private LinkedList<String> output = new  LinkedList<String>();
 
     //operator list add
     private HashSet<String> operators = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
@@ -192,8 +191,10 @@ public class calculatorViewController {
         if(sinceLastOperator != 0 && currentNumber != 0){
             firstInput.addLast(String.valueOf(currentNumber));
             resetCurrentNumber();
+            updateDisplay("=");
 
-            
+
+
         }
 
 
@@ -228,9 +229,86 @@ public class calculatorViewController {
                 case "/":
                     resultLabel.setText("/");
                     break;
+                case "=":
+                    resultLabel.setText(String.valueOf(compute()));
             }
         }
 
+    }
+
+    public int compute(){
+        //converts firstInput into RPN, then equates
+        var outputQueue = new LinkedList<String>();
+        var operatorStack = new LinkedList<String>();
+
+        for(int i = 0; i < firstInput.size(); i++){
+            //numbers are pushed directly into the outputQueue
+            if(!operators.contains(firstInput.get(i))){
+                outputQueue.add(firstInput.get(i));
+            }
+            //operators are added here based on scrutiny
+            else {
+                while(!operatorStack.isEmpty() && precedence(operatorStack.getLast()) >= precedence(firstInput.get(i))){
+                    outputQueue.add(operatorStack.removeLast()); //remove until no longer precedence
+                }
+                operatorStack.addLast(firstInput.get(i));
+            }
+        }
+        //pop any remaining operators
+        while(!operatorStack.isEmpty()){
+            outputQueue.add(operatorStack.removeLast());
+        }
+
+
+        //evaluate RPN in outputQueue and calculate
+        //operatorStack is now re-used to calculate
+        operatorStack.clear();
+        for(int i = 0; i < outputQueue.size(); i++){
+            if(!operators.contains(outputQueue.get(i))){
+                operatorStack.addLast(outputQueue.get(i));
+            }
+            else if(operators.contains(outputQueue.get(i))){
+                int secondNum = Integer.parseInt(operatorStack.removeLast());
+                int firstNum = Integer.parseInt(operatorStack.removeLast());
+                int result = 0;
+
+                switch(outputQueue.get(i)){
+                    case "+":
+                        result = firstNum + secondNum;
+                        break;
+                    case "-":
+                        result = firstNum - secondNum;
+                        break;
+
+                    case "*":
+                        result = firstNum * secondNum;
+                        break;
+
+                    case "/":
+                        result = firstNum / secondNum;
+                        break;
+
+                }
+                operatorStack.addLast(String.valueOf(result));
+            }
+        }
+        return Integer.parseInt(operatorStack.getLast());
+
+
+    }
+
+    public int precedence(String operator){
+        switch (operator){
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+            case "*":
+                return 2;
+            case "/":
+                return 2;
+        }
+        return 0; //this is a formality, logically this is never reached
     }
 
 
